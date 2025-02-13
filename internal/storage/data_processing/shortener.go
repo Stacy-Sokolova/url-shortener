907e1d08934c27d485c0d6b292a3cb87ca9a2408
Anastasia S <stacysv17@gmail.com>
2025-02-13 16:40:48 +0300

    fixed shortener function

--- internal/storage/data_processing/shortener.go
+++ internal/storage/data_processing/shortener.go
@@ -4,31 +4,27 @@ import (
 	"crypto/sha1"
 )
 
-var ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_" //63
+var ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
 
 func URLShortener(url string) string {
 	hash := sha1.New()
 	hash.Write([]byte(url))
-	s := hash.Sum(nil)[:10]
-	res := ""
-	for i := range s {
-		res += EncodeTo63(s[i])
+	s := hash.Sum(nil)
+	result := make([]byte, 10)
+	k := 9
+	for k >= 0 {
+		for i := range s {
+			if k < 0 {
+				break
+			}
+			num := s[i]
+			for num > 0 && k >= 0 {
+				digit := num % 63
+				result[k] = ALPHABET[digit]
+				k--
+				num = num / 63
+			}
+		}
 	}
-	//shortURL := base64.URLEncoding.EncodeToString(hash.Sum(nil))[:8]
-	//return fmt.Sprintf("%x", s)
-	return res[:10]
-}
-
-func EncodeTo63(num byte) string {
-	res := []string{}
-	for num > 0 {
-		digit := num % 63
-		res = append(res, string(ALPHABET[digit]))
-		num = num / 63
-	}
-	str := ""
-	for i := len(res) - 1; i >= 0; i-- {
-		str += res[i]
-	}
-	return str
+	return string(result)
 }
